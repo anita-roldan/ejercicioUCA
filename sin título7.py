2245d3940275f750f5451b2dc49a972d653865e0
anita-roldan <anaroldan@uca.edu.ar>
2025-04-03 18:36:15 -0300

    Add files via upload

---
+++ sin título7.py
@@ -0,0 +1,10 @@
+v1= input("ingrese valor 1:")
+
+v2= input("ingrese valor 2:")
+
+if v1==v2:
+    print("las variables son iguales")
+elif v1<v2:
+    print("la variable 2 es mayor que la variable 1")
+elif v1>v2:
+    print("la variable 1 es mayor que la variable 2")
